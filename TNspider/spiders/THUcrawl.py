52678d7a5034d395a45fff61bc24ea34d8a4f299
Journey-laptop <gaoqi0527@qq.com>
2014-08-23 22:41:52 +0800

    Tsinghua crawl added 	-modified:   TNspider/spiders/THUcrawl.py 		grub 2014 Tsinghua job info

--- TNspider/spiders/THUcrawl.py
+++ TNspider/spiders/THUcrawl.py
@@ -2,7 +2,9 @@
 import scrapy
 import string
 import time
-import simplejson 
+# import simplejson 
+import json
+import calendar
 # from selenium import webdriver
 from scrapy.selector import Selector
 from scrapy.http import Request
@@ -24,24 +26,46 @@ class TsucrawlSpider(scrapy.Spider):
         scrapy.Spider.__del__(self)
         
     def parse(self, response):
-        
-        ajaxRq="http://career.cic.tsinghua.edu.cn/xsglxt/b/jyxt/anony/queryTodayHdList?rq=2014-04-08&callback=jsonp1408781593994"
-        yield  Request(url=ajaxRq,callback=self.parse_item)
+            # ajaxRq="http://career.cic.tsinghua.edu.cn/xsglxt/b/jyxt/anony/queryTodayHdList?rq=2014-05-15&callback=jsonp1408781596000"
+            # yield  Request(url=ajaxRq,meta={'date': "2014-05-15"},callback=self.parse_item)
+        years = [2014]
+        for y in years:
+            for m in range(1,13):
+                dayboundary=calendar.monthrange(y,m)[1]#get day numbers in exact month
+                for d in range(1,dayboundary+1):
+                    ajaxRq="http://career.cic.tsinghua.edu.cn/xsglxt/b/jyxt/anony/queryTodayHdList?rq=%d-%02d-%02d&callback=jsonp14087815960%02d" % (y,m,d,d)
+                    yield  Request(url=ajaxRq,meta={'date': "%d-%02d-%02d" % (y,m,d)},callback=self.parse_item)
+                    
             
         
     def parse_item(self,response):
-        print response
         print response.body
-        result=response.body.split('[')
-        result=result[1].split(']')
-        print result[0]
-        
-        dictinfo = simplejson.loads(result[0])
-        result=simplejson.dumps(dictinfo)
+        result=response.body.split('(')
+        result=result[1].split(')')[0]
         print result
+        json2dict = eval('('+result+')')
+        if json2dict:
+            for dictt in json2dict:
+                print dictt
+                item = BNUItem()
+                item['date'] = response.meta['date']
+                item['university'] = 'Tsinghua'
+                item['city'] = 'Beijing'
+                item['title'] = dictt['bt']
+                item['time'] = dictt['kssj'],dictt['jssj']
+                item['addr'] = dictt['cdmcqc']
+                item['link'] = "http://career.cic.tsinghua.edu.cn/xsglxt/f/jyxt/anony/gotoZpxxList?id="+dictt['zphid']
+                yield Request(url=item['link'],meta={'item': item},callback=self.parse_info_item)
+        else:
+            print "Empty"
+            return
         
-        # item = response.meta['item']
-        # sel_detail = Selector(response)
-        # desc = sel_detail.xpath('//div[@class="i3001 job1list empnr intorone02"]/descendant::text()').extract()
-        # item['desc'] = [d.encode('UTF-8') for d in desc]
-        # yield item
+
+    def parse_info_item(self,response):
+        item = response.meta['item']
+        sel_detail = Selector(response)
+        desc = sel_detail.xpath('//li[@class="clearfix"]/p/a/text()').extract()
+        item['desc'] = [d.encode('UTF-8') for d in desc]
+        item['click_times'] = '0'
+        print "Done!"
+        yield item
